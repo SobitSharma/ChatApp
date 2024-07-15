7620bccc7775dfa6c4ff61a45ddf1524a972d2d6
SobitSharma <sharmashobit223@gmail.com>
2024-07-15 09:59:02 +0530

    bug fix

--- frontend/src/component/AllStatus.jsx
+++ frontend/src/component/AllStatus.jsx
@@ -1,59 +1,65 @@
-import React, { useEffect } from 'react'
-import {useNavigate} from "react-router-dom"
-import { useStatus } from '../Context/StatusContext'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useStatus } from "../Context/StatusContext";
+import { useUserContext } from "../Context/UserContext";
 
 function AllStatus() {
-    const navigate = useNavigate()
-    const {updateallStatusData, allStatusData} = useStatus()
-    const GetAllStatus = ()=> {
-        fetch("http://localhost:8000/api/status/getallstatus", {
-            method:'GET',
-            credentials:'include'
-        }).then((response)=>response.json()).
-        then((result)=>updateallStatusData(result.alluserStatus)).
-        catch((error)=>alert("Unexpected Error Ocurred"))
-    }
+  const navigate = useNavigate();
+  const {userLogin} = useUserContext()
+  const { updateallStatusData, allStatusData } = useStatus();
+  const GetAllStatus = () => {
+    fetch("http://localhost:8000/api/status/getallstatus", {
+      method: "GET",
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((result) => updateallStatusData(result.alluserStatus))
+      .catch((error) => alert("Unexpected Error Ocurred"));
+  };
 
-    useEffect(GetAllStatus, [])
+  useEffect(GetAllStatus, []);
   return (
     <div>
-  <div className="flex items-center justify-center min-h-screen bg-gray-100">
-    <div className="w-full max-w-2xl bg-gray-800 shadow-md rounded-lg p-4 overflow-y-auto h-96">
-      <div className='flex flex-row gap-96'>
-    <button
-    onClick={() => navigate("/mystatus")}
-    className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-  >
-    My Status
-  </button>
-  <button
-    onClick={() => navigate("/room")}
-    className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-  >
-    Back
-  </button>
-  </div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
       {
-        allStatusData?.map((singleStatus) => (
+        userLogin ? <div className="w-full max-w-2xl bg-gray-800 shadow-md rounded-lg p-4 overflow-y-auto h-96">
+        <div className="flex flex-row gap-96">
+          <button
+            onClick={() => navigate("/mystatus")}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            My Status
+          </button>
+          <button
+            onClick={() => navigate("/room")}
+            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Back
+          </button>
+        </div>
+        {allStatusData?.map((singleStatus) => (
           <div
             key={singleStatus._id}
             className="flex items-center border-b border-gray-200 py-2 justify-center hover:bg-gray-300"
-            onClick={()=>navigate("/showstatus", {state:{singleStatus}})}
+            onClick={() =>
+              navigate("/showstatus", { state: { singleStatus } })
+            }
           >
             <img
               src={singleStatus.statusId?.profilePic}
               alt={singleStatus.statusId?.fullname}
               className="w-10 h-10 rounded-full mr-4"
             />
-            <li className="list-none text-white">{singleStatus.statusId.fullname}</li>
+            <li className="list-none text-white">
+              {singleStatus.statusId.fullname}
+            </li>
           </div>
-        ))
+        ))}
+      </div> : <button className="bg-black text-white text-2xl p-3" onClick={()=>navigate('/login')}>Login</button>
       }
     </div>
   </div>
-</div>
-
-  )
+  );
 }
 
-export default AllStatus
+export default AllStatus;
--- frontend/src/component/ShowStatus.jsx
+++ frontend/src/component/ShowStatus.jsx
@@ -1,50 +1,52 @@
 import React, { useState } from 'react'
 import { useLocation, useNavigate } from 'react-router-dom';
+import { useUserContext } from '../Context/UserContext';
 
 function ShowStatus() {
     const [number, setnumber] = useState(0)
+    const {userLogin} = useUserContext()
     const location = useLocation()
     const navigate = useNavigate()
     const {singleStatus} = location.state || {}
-
-    console.log(singleStatus)
     
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div >
-        <div>
-          <button className='bg-black text-white p-4 rounded-lg text-xl' onClick={()=>navigate("/status")}>Back</button>
-        </div>
-        <div className="flex items-center gap-4">
-              <button
-                onClick={() => {
-                  if (number > 0) {
-                    setnumber((prev) => prev - 1);
-                  }
-                }}
-                className="bg-blue-500 text-white p-10 rounded hover:bg-blue-600 text-3xl"
-              >
-                {"<"}
-              </button>
-              <div className="max-w-3xl max-h-3xl bg-gray-700 flex items-center justify-center rounded-lg p-2">
-                <img
-                  src={singleStatus?.statusData[number]}
-                  alt="Not available"
-                  className="max-w-full max-h-full object-contain"
-                />
-              </div>
-              <button
-                onClick={() => {
-                  if (number < singleStatus?.statusData?.length - 1) {
-                    setnumber((prev) => prev + 1);
-                  }
-                }}
-                className="bg-blue-500 text-white p-10 rounded hover:bg-blue-600 text-3xl"
-              >
-                {">"}
-              </button>
+     {
+      userLogin ?  <div >
+      <div>
+        <button className='bg-black text-white p-4 rounded-lg text-xl' onClick={()=>navigate("/status")}>Back</button>
+      </div>
+      <div className="flex items-center gap-4">
+            <button
+              onClick={() => {
+                if (number > 0) {
+                  setnumber((prev) => prev - 1);
+                }
+              }}
+              className="bg-blue-500 text-white p-10 rounded hover:bg-blue-600 text-3xl"
+            >
+              {"<"}
+            </button>
+            <div className="max-w-3xl max-h-3xl bg-gray-700 flex items-center justify-center rounded-lg p-2">
+              <img
+                src={singleStatus?.statusData[number]}
+                alt="Not available"
+                className="max-w-full max-h-full object-contain"
+              />
             </div>
-            </div>
+            <button
+              onClick={() => {
+                if (number < singleStatus?.statusData?.length - 1) {
+                  setnumber((prev) => prev + 1);
+                }
+              }}
+              className="bg-blue-500 text-white p-10 rounded hover:bg-blue-600 text-3xl"
+            >
+              {">"}
+            </button>
+          </div>
+          </div> : <button className="bg-black text-white text-2xl p-3" onClick={()=>navigate('/login')}>Login</button>
+     }
     </div>
   )
 }
